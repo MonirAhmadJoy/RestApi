1b450da99ca3fc6cfe7743d101b40b0bf174c2d3
Monir Ahmad <87666860+MonirAhmadJoy@users.noreply.github.com>
2023-07-29 14:32:36 +0600

    Update apiController.java

--- app/src/main/java/com/example/rest/apiController.java
+++ app/src/main/java/com/example/rest/apiController.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class apiController
 {
-    private static final String url="http://192.168.43.110/api/";
+    private static final String url="http://IPv4 address/api/";
     private static apiController clientobject;
     private static Retrofit retrofit;
 
